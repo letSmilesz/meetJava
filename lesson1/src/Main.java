1423e541dd11ef54e2c8e0924260b2686ef55de8
letSmiles <lihachev647@gmail.com>
2023-01-21 21:11:39 +0300

    do all classwork

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
             } else if (task == 1) task1();
             else if (task == 2) task2();
             else if (task == 3) task3();
+            else if (task == 4) task4();
         }
     }
 
@@ -38,7 +39,7 @@ public class Main {
         System.out.println("Good " + timesOfDay + ", " + name);
     }
 
-    public static void task3() {
+    public static void task3() { //посчитать максимальное количесво единиц подряд
         System.out.println("Write length of array: ");
         int[] numbers = new int[10];//scanner.nextInt()];
         for (int i = 0; i < numbers.length; i++) {
@@ -56,6 +57,52 @@ public class Main {
         max_length = Math.max(max_length, count);
         printArray(numbers);
         System.out.println("Max length of number '1': " + max_length);
+    }
+
+    public static void task4() { //Дан массив nums = [3,2,2,3] и число val = 3.
+ // Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
+ // Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
+        int[] nums = new int[] {3, 2, 2, 3, 2, 2, 4, 7, 3, 3, 3, 4, 3};
+        int val = 3;
+        for (int i = 0; i < nums.length / 2; i++) {
+            if (nums[i] == val) {
+                int temp = 0;
+                for (int j = nums.length - 1; j > 0; j--) {
+                    if (nums[j] != val) {
+                        temp = j;
+                        break;
+                    }
+                }
+                int help = nums[i];
+                nums[i] = nums[temp];
+                nums[temp] = help;
+            }
+        }
+        Main.printArray(nums);
+    }
+
+    public static void task5 () { //Напишите метод, который находит самую длинную строку общего префикса среди массива строк.
+ //Если общего префикса нет, вернуть пустую строку "".
+        String[] str = new String[] {"flower","flow","floght", "florwhb"};
+        String prefix = "None";
+        int length = str[0].length();
+        for (int i = 1; i < str.length; i++) {
+            if (str[i].length() < length) length = str[i].length();
+        }
+        for (int i = 0; i < length; i++) {
+            int count = 1;
+            char letter = str[0].charAt(i);
+            for (int j = 1; j < str.length; j++) {
+                if (str[j].charAt(i) == letter) count++;
+                else break;
+            }
+            if (count == str.length) {
+                if (prefix.equals("None")) prefix = "";
+                prefix += letter;
+            }
+            else break;
+        }
+        System.out.println(prefix);
     }
 
     static void printArray(int[] array) {
