9c870a02a35c2bc131cdb04761e9597c2a91d4ba
letSmiles <lihachev647@gmail.com>
2023-01-27 23:41:16 +0300

    do task 1

--- lesson3/src/Task1.java
+++ lesson3/src/Task1.java
@@ -1,2 +1,20 @@
+import java.util.*;
+
 public class Task1 {
+    static Random r = new Random();
+    public static void main(String[] args) {
+        int length = r.nextInt(10, 20);
+        List<Integer> list = fillList(length);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+    static ArrayList<Integer> fillList (int length) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            arr.add(r.nextInt(0,20));
+        }
+        return arr;
+    }
 }
