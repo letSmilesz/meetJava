e8dd0c2a6df2a99f5e865573c20ab308e88ac80e
letSmiles <lihachev647@gmail.com>
2023-01-28 01:58:54 +0300

    do all tasks

--- lesson3/src/Main.java
+++ lesson3/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true){
+            System.out.println("What task do you want to run? (to exit enter 0): ");
+            int task = scanner.nextInt();
+            scanner.nextLine();
+            if (task == 0) {
+                scanner.close();
+                break;
+            } else if (task == 1) {
+                Task1.main(args);
+            } else if (task == 2) {
+                Task2.main(args);
+            } else if (task == 3) {
+                Task3.main(args);
+            } else {
+                Task4.main(args);
+            }
+        }
     }
 }
